8802205b3cd45e932d90f619ddaee4d55ed5f1bf
John Marcelo <johntec.ads@gmail.com>
2024-08-13 21:38:17 -0300

    upProject

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import { useState } from 'react';
-import Alunos from './components/Alunos';
 
-import UserProvider from './context/user';
+
+
+
 
 function App () {
-  
-
 
   return (
-
-    <UserProvider>
-      <div>
-        <h1> Escola </h1>
-
-        <hr />
-        <Alunos />
-
-      </div>
-    </UserProvider>
-
+    <h1> Teste </h1>    
 
   );
 }
