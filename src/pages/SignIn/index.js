e478b18e635bf19bf4bb4a27d721201d3d502854
John Marcelo <johntec.ads@gmail.com>
2024-08-15 08:01:58 -0300

    importLogo

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,9 +1,20 @@
 import './signin.css'
+import logo from '../../assets/logo.png'
 
 export default function SignIn () {
   return (
   <div className='container-center' >
+
+    <div className='login'>
+
+      <div className='login-area' >
+        <img src={logo} alt='Logo do sistema de chamadas' />
+      </div>
+
+    </div>
+
     <h1> Página login </h1>
+
   </div>
 )
 }
