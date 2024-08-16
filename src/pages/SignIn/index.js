c401ce74f3e1ee38f9b34466f7b214707904a2df
John Marcelo <johntec.ads@gmail.com>
2024-08-15 21:49:02 -0300

    signinSignup

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,14 +1,12 @@
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
-import './signin.css'
 import logo from '../../assets/logo.png'
+import './signin.css'
 
 
 export default function SignIn () {
-
   const [ email, setEmail ] = useState( '' );
   const [ password, setPassword ] = useState( '' );
-
 
   return (
     <div className='container-center' >
@@ -16,30 +14,24 @@ export default function SignIn () {
         <div className='login-area' >
           <img src={ logo } alt='Logo do sistema de chamadas' />
         </div>
+        <form>
+          <h1> Entrar </h1>
+          <input
+            type='text'
+            placeholder='email@email.com'
+            value={ email }
+            onChange={ ( e ) => setEmail( e.target.value ) }
+          />
+          <input
+            type='password'
+            placeholder='digite sua senha'
+            value={ password }
+            onChange={ ( e ) => setPassword( e.target.value ) }
+          />         
+          <button type='submit'>Acessar</button>
+        </form>
+        <Link to='/register' >Criar uma conta </Link>
       </div>
-
-      <form>
-        <h1> Entrar </h1>
-        <input
-          type='text'
-          placeholder='email@email.com'
-          value={email}
-          onChange={ ( e ) => setEmail( e.target.value ) }
-        />
-        <input
-          type='password'
-          placeholder='digite sua senha'
-          value={password}
-          onChange={ ( e ) => setPassword( e.target.value ) }
-        />
-        <input type='submit' value='Acessar'/>
-
-      </form>
-      <Link to='/register' >Criar uma conta </Link>
-
-      
-
-
     </div>
   )
 }
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,8 +1,46 @@
+import logo from '../../assets/logo.png'
+
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import './signup.css'
+
 
 export default function SignUp () {
+  const [ email, setEmail ] = useState( '' );
+  const [ password, setPassword ] = useState( '' );
+  const [ name, setName ] = useState('');
+
   return (
-  <div>
-    <h1> Página   Cadastro </h1>
-  </div>
-)
+    <div className='container-center' >
+      <div className='login'>
+        <div className='login-area' >
+          <img src={ logo } alt='Logo do sistema de chamadas' />
+        </div>
+        <form>
+          <h1> Nova conta </h1>
+
+          <input
+            type='text'
+            placeholder='Nome'
+            value={ name }
+            onChange={ ( e ) => setName( e.target.value ) }
+          />
+          <input
+            type='text'
+            placeholder='email@email.com'
+            value={ email }
+            onChange={ ( e ) => setEmail( e.target.value ) }
+          />
+          <input
+            type='password'
+            placeholder='digite sua senha'
+            value={ password }
+            onChange={ ( e ) => setPassword( e.target.value ) }
+          />         
+          <button type='submit'>Cadastrar</button>
+        </form>
+        <Link to='/' >Já possui uma conta ? Faça login.</Link>
+      </div>
+    </div>
+  )
 }
