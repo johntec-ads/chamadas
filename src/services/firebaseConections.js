22ee7f002a8b45be05cecb00470a476563d46efa
johntec-ads <johntec.ads@gmail.com>
2024-08-20 06:43:44 -0300

    firebaseConfig

---
+++ src/services/firebaseConections.js
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { Firestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDwPWBVS-ieuC8c1Baocf8phXbnrF0hSS4",
+  authDomain: "tickets-f290a.firebaseapp.com",
+  projectId: "tickets-f290a",
+  storageBucket: "tickets-f290a.appspot.com",
+  messagingSenderId: "162146573208",
+  appId: "1:162146573208:web:f8a5be855910c9171d29ed",
+  measurementId: "G-31FNJPET04"
+};
