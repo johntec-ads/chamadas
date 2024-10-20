32fb174d291e759aa08dc99283e1f32ba6954dca
John Marcelo <johntec.ads@gmail.com>
2024-10-20 17:55:03 -0300

    upProjectChamados

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ import {Routes, Route} from 'react-router-dom';
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 import Dashboard from '../pages/Dashboard';
+import Profile from '../pages/Profile'
 
 import Private from './Private';
 
@@ -11,6 +12,7 @@ function RoutesApp () {
     <Routes>
       <Route path='/' element={ <SignIn /> } />
       <Route path='/register' element={ <SignUp /> } />
+      <Route path='/profile' element={ <Private> <Profile/> </Private> } />
       
       <Route path='/dashboard' element={ <Private> <Dashboard/> </Private> } />
     </Routes>
