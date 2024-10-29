ac956edc48a837f13771a394f6504cc964e05310
John Marcelo <johntec.ads@gmail.com>
2024-10-29 06:25:00 -0300

    upProject

---
+++ src/pages/Customers/index.js
@@ -0,0 +1,29 @@
+import Header from '../../components/Header';
+import Title from '../../components/Title';
+
+import { FiUser } from 'react-icons/fi';
+
+
+export default function Customers () {
+
+  return(
+    <div>
+    <Header/>
+
+    <div className='content'>
+
+      <Title name='Clientes'>
+        <FiUser size={25} />
+      </Title>
+
+      <div className='container' >
+        <form className='form-porfile' >
+
+        </form>
+      </div>
+
+    </div>    
+
+    </div>
+  )
+}
