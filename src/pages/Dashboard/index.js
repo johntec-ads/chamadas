5cb6cfce4c135803f952ad3105f98d7f7e5f357a
John Marcelo <johntec.ads@gmail.com>
2024-11-02 08:52:50 -0300

    upGradTable

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -3,7 +3,7 @@ import { AuthContext } from '../../contexts/auth';
 
 import Header from '../../components/Header';//import do component Header
 import Title from '../../components/Title';
-import { FiPlus, FiMessageSquare } from 'react-icons/fi';
+import { FiPlus, FiMessageSquare, FiSearch, FiEdit2 } from 'react-icons/fi';
 
 import { Link } from 'react-router-dom';
 
@@ -31,9 +31,34 @@ export default function Dashboard () {
             <FiPlus color='#FFF' size={25} />
             Novo chamado
           </Link>
-            <div className='container' >
-              <h1>Teste</h1>
-            </div>
+            <table>
+              <thead>
+                <tr>
+                  <th scope='col' >Cliente</th>
+                  <th scope='col' >Assunto</th>
+                  <th scope='col' >Status</th>
+                  <th scope='col' >Cadastrado em</th>
+                  <th scope='col' >#</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td data-label='Cliente' >Mercado Esquina</td>
+                  <td data-label='Assunto' >Suporte</td>
+                  <td data-label='Status' >Em Aberto</td>
+                  <td data-label='Cadastrado' >12/05/2024</td>
+                  <td data-label='#' >
+                    <button className='action' style={{backgroundColor: '#3583f6'}} >
+                      <FiSearch color='#FFF' size={17} />
+                    </button>
+                    <button className='action' style={{backgroundColor: '#f6a935'}} >
+                      <FiEdit2 color='#FFF' size={17} />
+                    </button>
+
+                  </td>
+                </tr>
+              </tbody>
+            </table>
         </>
 
       </div>
