380b3a8eb39b851f720108c1e29c569d0a57a4f3
John Marcelo <johntec.ads@gmail.com>
2024-11-06 06:26:24 -0300

    upProject

--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -9,27 +9,59 @@ import { collection, getDocs, getDoc, doc } from 'firebase/firestore';
 
 import './new.css';
 
-const listRef = collection(db, 'customers')
+const listRef = collection( db, 'customers' );
 
 export default function New () {
-  const { user } = useContext(AuthContext);
+  const { user } = useContext( AuthContext );
 
   const [ customers, setCustomers ] = useState( [] );/*Array p/ lista de cliêntes */
   const [ loadCustomer, setLoadCustomer ] = useState( true );
+  const [ customerSelected, setCustomerSelected ] = useState( 0 );
 
 
   const [ complemento, setComplemento ] = useState( '' );
   const [ assunto, setAssunto ] = useState( 'Suporte' );
   const [ status, setStatus ] = useState( 'Aberto' );
 
-  useEffect(() => {
-    async function loadCustomers() {
-      const querySnapshot = await getDocs(listRef)   
-    }
+  useEffect( () => {
+    async function loadCustomers () {
+      const querySnapshot = await getDocs( listRef )
+        .then( ( snapshot ) => {/* promisse caso de sucesso */
+          /* percorre o docs buscando o id e ref, e adiciona para useState customers */
+          let lista = [];
 
+          snapshot.forEach( ( doc ) => {/*repassa todos os documentos*/
+            lista.push( {
+              id: doc.id,
+              nomeFantasia: doc.data().nomeFantasia
+            } )
+          } )
+
+          if ( snapshot.docs.size === 0 ) {/* verifica se esta vazio */
+            console.log( 'NENHUMA EMPRESA ENCONTRADA' );
+            /* se vazio, preenche com cliênte fictício */
+            setCustomers( [ { id: '1', nomeFantasia: 'FREELA' } ] )
+            setLoadCustomer( false );
+            return;/* para o código. */
+          }
+
+          /* Encontrou os itens */
+          setCustomers( lista );/* lista atualizada */
+          setLoadCustomer( false );/* para o código */
+
+        } )
+        .catch( ( error ) => {/* caso de erro */
+          console.log( "Erro ao buscar os clientes", error )
+          setLoadCustomer( false );
+          setCustomers(
+            /* Em caso de erro, vamos criar um cliênte fictício */
+            [ { id: '1', nomeFantasia: 'FREELA' } ]
+          )
+        } )
+    }
     loadCustomers();
-    
-  }, [])
+
+  }, []/* array de dependencias */ )
 
   function handleOptionChange ( e ) {
     setStatus( e.target.value )
@@ -37,6 +69,11 @@ export default function New () {
 
   function handleChangeSelect ( e ) {
     setAssunto( e.target.value );
+  }
+
+  function handleChangeCustomer(e) {
+    setCustomerSelected(e.target.value)
+
   }
 
 
@@ -53,10 +90,17 @@ export default function New () {
           <form className='form-profile'>
 
             <label>Clientes</label>
-            <select>
-              <option key={ 1 } value={ 1 } >Mercado teste</option>
-              <option key={ 2 } value={ 2 } >Loja informatica</option>
-            </select>
+            {
+              /* Se loadCustomer estiver true, vai renderizar o input */
+              loadCustomer ? (
+                <input type='text' disabled={ true } value='Carregando...' />
+              ) : (
+                /* Se loadCustomer não estiver true */
+                <select value={ customerSelected } onChange={handleChangeCustomer} >
+
+                </select>
+              )
+            }
 
             <label>Assunto</label>
             <select value={ assunto } onChange={ handleChangeSelect } >
