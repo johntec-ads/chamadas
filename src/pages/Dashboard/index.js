ce577771cfc20a74ce1a1d31d74913a06fedd12c
John Marcelo <johntec.ads@gmail.com>
2024-11-08 06:30:26 -0300

    listArrayChamados

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../contexts/auth';
 
 import Header from '../../components/Header';//import do component Header
@@ -6,16 +6,22 @@ import Title from '../../components/Title';
 import { FiPlus, FiMessageSquare, FiSearch, FiEdit2 } from 'react-icons/fi';
 
 import { Link } from 'react-router-dom';
+import { collection, getDocs, orderBy, limit, startAfter } from 'firebase/firestore'
 
 import './dashboard.css';
 
 export default function Dashboard () {
   const { logout } = useContext( AuthContext );
 
-  async function handleLogout () {
-    await logout()
-  }
+  const [ chamados, setChamados ] = useState( [] );
+  const [ loading, setLoading ] = useState( true );
 
+  useEffect(() => {
+    async function loadChamados() {
+      
+    }
+    loadChamados()
+  }, [])
 
   return (
     <div>
@@ -27,60 +33,59 @@ export default function Dashboard () {
         </Title>
 
         <>
-          <Link to="/new" className='new'>
-            <FiPlus color='#FFF' size={ 25 } />
-            Novo chamado
-          </Link>
-          <table>
-            <thead>
-              <tr>
-                <th scope='col' >Cliente</th>
-                <th scope='col' >Assunto</th>
-                <th scope='col' >Status</th>
-                <th scope='col' >Cadastrado em</th>
-                <th scope='col' >#</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td data-label='Cliente' >Mercado Rocha</td>
-                <td data-label='Assunto' >Suporte</td>
-                <td data-label='Status' >
-                  <span className='badge'style={{backgroundColor: '#999'}} >
-                  Em Aberto
-                  </span>
-                </td>
-                <td data-label='Cadastrado' >12/05/2024</td>
-                <td data-label='#' >
-                  <button className='action' style={ { backgroundColor: '#3583f6' } } >
-                    <FiSearch color='#FFF' size={ 17 } />
-                  </button>
-                  <button className='action' style={ { backgroundColor: '#f6a935' } } >
-                    <FiEdit2 color='#FFF' size={ 17 } />
-                  </button>
-                </td>
-              </tr>
 
-              <tr>
-                <td data-label='Cliente' >InfoTech</td>
-                <td data-label='Assunto' >Suporte</td>
-                <td data-label='Status' >
-                  <span className='badge'style={{backgroundColor: '#999'}} >
-                  Em Aberto
-                  </span>
-                </td>
-                <td data-label='Cadastrado' >12/05/2024</td>
-                <td data-label='#' >
-                  <button className='action' style={ { backgroundColor: '#3583f6' } } >
-                    <FiSearch color='#FFF' size={ 17 } />
-                  </button>
-                  <button className='action' style={ { backgroundColor: '#f6a935' } } >
-                    <FiEdit2 color='#FFF' size={ 17 } />
-                  </button>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+          { chamados.length === 0 ? (/* Se for igual a 0, não tem chamados*/
+            <div className='container dashboard' >
+              <span>Nehum chamado encontado... </span>
+              <Link to="/new" className='new'>
+                <FiPlus color='#FFF' size={ 25 } />
+                Novo chamado
+              </Link>
+            </div>
+          ) : (
+            <>
+              <Link to="/new" className='new'>
+                <FiPlus color='#FFF' size={ 25 } />
+                Novo chamado
+              </Link>
+
+              <table>
+                <thead>
+                  <tr>
+                    <th scope='col' >Cliente</th>
+                    <th scope='col' >Assunto</th>
+                    <th scope='col' >Status</th>
+                    <th scope='col' >Cadastrado em</th>
+                    <th scope='col' >#</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td data-label='Cliente' >Mercado Rocha</td>
+                    <td data-label='Assunto' >Suporte</td>
+                    <td data-label='Status' >
+                      <span className='badge' style={ { backgroundColor: '#999' } } >
+                        Em Aberto
+                      </span>
+                    </td>
+                    <td data-label='Cadastrado' >12/05/2024</td>
+                    <td data-label='#' >
+                      <button className='action' style={ { backgroundColor: '#3583f6' } } >
+                        <FiSearch color='#FFF' size={ 17 } />
+                      </button>
+                      <button className='action' style={ { backgroundColor: '#f6a935' } } >
+                        <FiEdit2 color='#FFF' size={ 17 } />
+                      </button>
+                    </td>
+                  </tr>
+
+
+                </tbody>
+              </table>
+            </>
+          ) }
+
+
         </>
 
       </div>
